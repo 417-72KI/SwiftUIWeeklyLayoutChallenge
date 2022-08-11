d6c4cdf00bbc622fadadbd9b9be944fb5094def3
417-72KI <417.72ki@gmail.com>
2022-08-12 00:25:45 +0900

    fix preview modifier

--- Sources/SwiftUIWeeklyLayoutChallenge/Util/PreviewModifier.swift
+++ Sources/SwiftUIWeeklyLayoutChallenge/Util/PreviewModifier.swift
@@ -1,25 +1,49 @@
 import SwiftUI
 
 struct PreviewModifier: ViewModifier {
+    var devices = Device.allCases
+    var colorSchemes = ColorScheme.allCases
+
     func body(content: Content) -> some View {
-        ForEach(devices, id: \.rawValue) { device in
-            if device.rawValue == "Mac" {
-                content.previewDevice(device)
-                    .frame(width: 320, height: 240, alignment: .center)
-            } else {
-                content.previewDevice(device)
+        ForEach(devices) { device in
+            ForEach(colorSchemes, id: \.self) { colorScheme in
+                content.previewDevice(device.previewDevice)
+                    .preferredColorScheme(colorScheme)
+                    .if(device == .mac) { $0.frame(minWidth: 320, minHeight: 240) }
             }
         }
     }
 }
 
-private extension PreviewModifier {
-    var devices: [PreviewDevice] {
-        [
-            "Mac",
-            "iPhone 13 mini",
-            "Apple TV 4K (2nd generation)",
-            "Apple Watch Series 7 - 45mm"
-        ]
+// MARK: -
+extension PreviewModifier {
+    enum Device: CaseIterable {
+        case mac
+        case iphone
+        case appleTV
+        case appleWatch
+    }
+}
+
+extension PreviewModifier.Device {
+    var previewDevice: PreviewDevice {
+        switch self {
+        case .mac: return "Mac"
+        case .iphone: return "iPhone 13 mini"
+        case .appleTV: return "Apple TV 4K (2nd generation)"
+        case .appleWatch: return "Apple Watch Series 7 - 45mm"
+        }
+    }
+}
+
+extension PreviewModifier.Device: Identifiable {
+    var id: String { previewDevice.rawValue }
+}
+
+// MARK: -
+extension View {
+    func previewDevices(_ devices: [PreviewModifier.Device] = PreviewModifier.Device.allCases,
+                        colorSchemes: [ColorScheme] = ColorScheme.allCases) -> some View {
+        modifier(PreviewModifier(devices: devices, colorSchemes: colorSchemes))
     }
 }
